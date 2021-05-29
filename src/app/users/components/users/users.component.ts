f1ca73d46b800e0fc56e810ceedf444fd68300de
DavidCZ94 <david940103@gmail.com>
2021-05-29 18:08:09 -0500

    pagination actions to users added

--- src/app/users/components/users/users.component.ts
+++ src/app/users/components/users/users.component.ts
@@ -14,6 +14,9 @@ export class UsersComponent implements OnInit {
   users: User[] = [];
   faPlus = faPlus;
   search: String = '';
+  nPerPage: number = 10;
+  pageNumber: number = 1;
+  isThereMoreData = true;
 
   constructor(
     private usersService: UsersService
@@ -24,14 +27,41 @@ export class UsersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  jumpToPage(pageNumber){
+    this.users = [];
+    if( pageNumber > 0 ){
+      this.pageNumber = pageNumber;
+      this.loadUsers();
+    }
+  }
+
+  nextPage(){
+    this.users = [];
+    this.pageNumber++;
+    this.loadUsers();
+  }
+
+  previousPage(){
+    if( this.pageNumber > 1 ){
+      this.users = [];
+      this.pageNumber--;
+      this.loadUsers();
+    }
+  }
+
   loadUsers(){
     const filter =
       typeof this.search == 'string' && this.search.length > 0
-        ? `?searchBy=${this.search}`
-        : ''
+        ? `?searchBy=${this.search}&pageNumber=${this.pageNumber}&nPerPage=${this.nPerPage}`
+        : `?pageNumber=${this.pageNumber}&nPerPage=${this.nPerPage}`
       this.usersService.getUsers(filter).subscribe(
         (res) => {
           this.users = res.data;
+          if(res.data.length < this.nPerPage ){
+            this.isThereMoreData = false;
+           }else{
+             this.isThereMoreData = true;
+           }
         },
         (err) =>{
           alert('An error occurred connecting to the database.');
