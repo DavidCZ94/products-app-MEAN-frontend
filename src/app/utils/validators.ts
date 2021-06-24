52a53084acc7a074bd410fefd5bd8a788ff68078
DavidCZ94 <david940103@gmail.com>
2021-06-24 11:10:42 -0500

    Order detail table adjusted. pasword validator adjusted

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -4,18 +4,12 @@ export class MyValidators {
 
   static validPassword(control: AbstractControl){
     const value = control.value;
-    if(!containsNumber(value)){
+    const upperCaseRegex = /[A-Z]/;
+    const lowerCaseRegex = /[a-z]/;
+    const numberRegex =/\d/;
+    if( !( (upperCaseRegex.test(value) || lowerCaseRegex.test(value)) && (numberRegex.test(value)) ) ){
         return {invalid_password: true}
     }
     return null;
   }
-  
 }
-
-function containsNumber(value: string){
-    return value.split('').find( v => isNumber(v) ) !== undefined;
-}
-
-function isNumber(value:string) {
-    return !isNaN(parseInt(value,10))
-} 
