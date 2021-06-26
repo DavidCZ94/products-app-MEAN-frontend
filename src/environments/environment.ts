25f634d72cb9bd8217ca03d77d1a9122928e85e1
DavidCZ94 <david940103@gmail.com>
2021-06-26 12:43:18 -0500

    git ignored Adjusted

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl:'https://products-app-mean-proxy-server.herokuapp.com',
-  cloudinaryUploadPreset: 'products-app-MEAN',
-  cloudinaryCloudName: 'dfm3qqywb',
-  cloudinaryApi: '/v1_1/'
-  //cloudinaryApi: 'https://api.cloudinary.com/v1_1/'
-};
