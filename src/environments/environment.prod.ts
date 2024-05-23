038fa03c65ac176e30f36abdab0e267c54fa4e1a
DavidCZ94 <david940103@gmail.com>
2024-05-22 23:45:10 -0500

    environment variables adjusted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'https://products-app-mean-proxy-server.herokuapp.com',
+  apiUrl:'https://products-app-mean-proxy-server.vercel.app',
   cloudinaryUploadPreset: 'products-app-MEAN',
   cloudinaryCloudName: 'dfm3qqywb',
   cloudinaryApi: 'https://api.cloudinary.com/v1_1/'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl:'https://products-app-mean-proxy-server.herokuapp.com',
+  apiUrl:'http://localhost:8000',
   cloudinaryUploadPreset: 'products-app-MEAN',
   cloudinaryCloudName: 'dfm3qqywb',
   cloudinaryApi: '/v1_1/'
